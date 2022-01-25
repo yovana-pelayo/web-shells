9795c6409ea87c22ad09fe2ef50b7689223dcb86
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-01-25 09:43:24 -0800

    added images to browser and defined containers

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // import functions and grab DOM elements
 
 // let state
+const espressoButton = document.getElementById('espresso-button');
+const americanoButton = document.getElementById('americano-button');
+const frappeButton = document.getElementById('frappe-button');
+
+const espressoContainer = document.getElementById('espresso-container');
+const americanoContainer = document.getElementById('espresso-container');
+const frappeContainer = document.getElementById('frappe-Container');
+
+const totalEl = document.getElementById('total');
+const lossesEl = document.getElementById('losses');
+const winsEl = document.getElementById('wins');
 
 // set event listeners 
   // get user input
